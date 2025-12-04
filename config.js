aab0fc5b2af33b924378add504b93160bccd415b
riyad022tagiyevv <97640853+riyad022tagiyevv@users.noreply.github.com>
2025-12-04 15:56:15 +0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  whatsappApiUrl: "https://api.ultramsg.com/YOUR_INSTANCE/messages",
+  token: "YOUR_TOKEN",
+
+  // Bu nömrələri dəyişəcəksən
+  customerReceiver: "+99455XXXXXXX",
+  donnerciReceiver: "+99455YYYYYYY"
+};
